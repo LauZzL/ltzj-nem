7e190aa939b254ecaafbbe015d0e113be92b5ec3
LauZzL <lauzzl@163.com>
2025-01-21 15:51:46 +0800

    ✨feat(utils/api.ts): 完善日常功能

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,48 +1,37 @@
 import {requests} from './requests.ts'
 import {useLoggerStore} from "@/store/logger.ts";
 import {useUserStore} from "@/store/user.ts";
+import {useSettingStore} from "@/store/setting.ts";
 import {gain as Gain} from "@/utils/gain.ts";
 import common from "@/utils/common.ts";
+import {PAYLOADS} from "@/payloads";
 
 const {log} = useLoggerStore()
 const userStore = useUserStore()
+const settingStore = useSettingStore()
 
 /**
  * 显示奖励，传入gain文本
  * @param gain
  */
 const _logGain = (gain: string) => {
-    const gain_json_array = Gain.gain2jsonarray(gain)
+    // 部分关卡返回金币奖励以#分割
+    const _gain = gain.replace(/#/g, ',')
+    const gain_json_array = Gain.gain2jsonarray(_gain)
     gain_json_array.map((g) => {
         log('success', `${g.name} x ${g.num}`, g.color, g.icon)
     })
 }
 
-export const api = {
+export const api: Record<string, any> = {
 
     /**
      * 寻宝转盘
      */
     dailyWheelReward: async (args: { num: number }) => {
-        console.debug(`Received parameters: ${args}`)
         for (let i = 0; i < args.num; i++) {
             log('processing', `寻宝转盘第${i + 1}次`)
-            const payload = {
-                "head": {
-                    "cmdDataSplitLength": 0,
-                    "cmdId": 64,
-                    "cmdLength": 0,
-                    "cmdSequence": 0,
-                    "cmdVersion": 20,
-                    "headVersion": 0,
-                    "timestamp": 0,
-                    "crcVerify": 0,
-                    "platform": 0,
-                    "reconnect": false,
-                    "sid": "",
-                    "uid": ""
-                }
-            }
+            const payload = PAYLOADS['dailyWheelReward']
             let result = await requests.pf(payload)
             if (result.head.state == 0) {
                 log('success', `寻宝转盘成功`)
@@ -55,7 +44,11 @@ export const api = {
         log('processing', `寻宝转盘结束`)
     },
 
-    dailyBlueBoxReward: async (args: { num: number, type: number }) => {
+    /**
+     * 装备宝箱
+     * @param args
+     */
+    dailyEquipBoxReward: async (args: { num: number, type: number }) => {
         async function exec(num: number) {
             let execNum = 0;
             if (num < 10 && num > 0) {
@@ -66,25 +59,10 @@ export const api = {
             if (execNum === 0) {
                 return;
             }
-            const payload = {
-                "head": {
-                    "cmdDataSplitLength": 0,
-                    "cmdId": 11,
-                    "cmdLength": 0,
-                    "cmdSequence": 0,
-                    "cmdVersion": 20,
-                    "headVersion": 0,
-                    "timestamp": 0,
-                    "crcVerify": 0,
-                    "platform": 0,
-                    "reconnect": false,
-                    "sid": "",
-                    "uid": ""
-                },
-                "shopIndex": 4,
-                "sdTargetData": {"type": 12, "sd_id": args.type, "quality": 4, "level": 0, "num": execNum},
-                "adBuy": "0"
-            };
+            log('processing', `本次开启${execNum}个宝箱,剩余${num}次等待开启中`);
+            const payload = PAYLOADS['dailyEquipBoxReward'];
+            payload.sdTargetData.num = execNum;
+            payload.sdTargetData.sd_id = args.type;
             let result = await requests.pf(payload);
             if (result.head.state === 0) {
                 log('success', `开启成功`);
@@ -100,23 +78,11 @@ export const api = {
         await exec(args.num);
     },
 
+    /**
+     * 刷新用户信息
+     */
     refreshUserInfo: async () => {
-        const payload = {
-            "head": {
-                "cmdDataSplitLength": 0,
-                "cmdId": 1,
-                "cmdLength": 0,
-                "cmdSequence": 1,
-                "cmdVersion": 20,
-                "headVersion": 0,
-                "timestamp": 0,
-                "crcVerify": 0,
-                "platform": 0,
-                "reconnect": false,
-                "sid": "",
-                "uid": ""
-            }
-        }
+        const payload = PAYLOADS['refreshUserInfo']
         let result = await requests.pf(payload)
         if (result.head.state == 0) {
             userStore.user = result
@@ -125,4 +91,248 @@ export const api = {
             log('error', `刷新用户信息失败:${result.head.msg}`)
         }
     },
+
+    /**
+     * 每日商店免费
+     */
+    dailyShopFree: async () => {
+        const payloads: { name: string, num: number, data: any }[] = PAYLOADS['dailyShopFree']
+        log('processing', `开始领取商店广告资源`)
+        for (const payload of payloads) {
+            log('processing', `开始领取${payload.name}`)
+            for (let i = 0; i < payload.num; i++) {
+                log('processing', `领取${payload.name}第${i + 1}次`)
+                let result = await requests.pf(payload.data)
+                if (result.head.state == 0) {
+                    log('success', `领取${payload.name}成功`)
+                    _logGain(result.head.gain)
+                } else {
+                    // 领取失败，跳出循环
+                    result.head.state == 164 ? log('error', `领取${payload.name}失败:无领取次数`) : log('error', `领取${payload.name}失败:${result.head.msg}`)
+                    break
+                }
+                await common.sleep(1000)
+            }
+            await common.sleep(1000)
+        }
+        log('processing', `领取商店广告资源结束`)
+    },
+
+    /**
+     * 每日广告体力
+     */
+    dailyEnergyReward: async (args: { num: number }) => {
+        const payload = PAYLOADS['dailyEnergyReward']
+        log('processing', `开始领取每日广告体力`)
+        for (let i = 0; i < args.num; i++) {
+            log('processing', `领取每日广告体力第${i + 1}次`)
+            let result = await requests.pf(payload)
+            if (result.head.state == 0) {
+                log('success', `领取每日广告体力成功`)
+                _logGain(result.head.gain)
+            } else {
+                result.head.state == 164 ? log('error', `领取每日广告体力失败:无领取次数`) : log('error', `领取每日广告体力失败:${result.head.msg}`)
+                break
+            }
+            await common.sleep(1000)
+        }
+    },
+
+    /**
+     * 寻宝装备宝箱
+     */
+    dailyEquipReward: async () => {
+        const payloads = PAYLOADS['dailyEquipReward']
+        log('processing', `开始领取寻宝装备宝箱`)
+        for (const payload of payloads) {
+            log('processing', `开始领取${payload.name}`)
+            let result = await requests.pf(payload.data)
+            if (result.head.state == 0) {
+                log('success', `领取${payload.name}成功`)
+                _logGain(result.head.gain)
+            } else {
+                result.head.state == 72 ? log('error', `领取${payload.name}失败:无领取次数`) : log('error', `领取${payload.name}失败:${result.head.msg}`)
+            }
+            await common.sleep(1000)
+        }
+    },
+
+    /**
+     * 星际探索
+     */
+    dailyExploreReward: async () => {
+        const payloads = PAYLOADS['dailyExploreReward']
+        log('processing', `开始领取星际探索`)
+        for (const payload of payloads) {
+            log('processing', `开始领取${payload.name}`)
+            for (let i = 0; i < payload.num; i++) {
+                log('processing', `领取${payload.name}第${i + 1}次`)
+                let result = await requests.pf(payload.data)
+                if (result.head.state == 0) {
+                    log('success', `领取${payload.name}成功`)
+                    _logGain(result.head.gain)
+                } else {
+                    (result.head.state == 67 || result.head.state == 155) ? log('error', `领取${payload.name}失败:无领取次数`) : log('error', `领取${payload.name}失败:${result.head.msg}`)
+                    break
+                }
+                await common.sleep(1000)
+            }
+            await common.sleep(1000)
+        }
+    },
+
+    /**
+     * 发送弹幕20钻石
+     */
+    sendDanMu: async () => {
+        const payload = PAYLOADS['sendDanMu']
+        log('processing', `开始发送弹幕`)
+        let result = await requests.pf(payload)
+        if (result.head.state == 0) {
+            log('success', `发送弹幕成功`)
+            _logGain(result.head.gain)
+        } else {
+            log('error', `发送弹幕失败:${result.head.msg}`)
+        }
+    },
+
+    /**
+     * 桌面启动钻石
+     */
+    desktopStart: async () => {
+        const payload = PAYLOADS['desktopStart']
+        log('processing', `开始领取桌面启动钻石`)
+        let result = await requests.pf(payload)
+        if (result.head.state == 0) {
+            log('success', `领取成功`)
+            _logGain(result.head.gain)
+        } else {
+            log('error', `领取失败:${result.head.msg}`)
+        }
+    },
+
+    /**
+     * 助战金币领取
+     */
+    dailyHelpGold: async () => {
+        const payload = PAYLOADS['dailyHelpGold']
+        log('processing', `开始领取助战金币`)
+        let result = await requests.pf(payload)
+        if (result.head.state == 0) {
+            log('success', `领取成功`)
+            _logGain(result.head.gain)
+        } else {
+            log('error', `领取失败:${result.head.msg}`)
+        }
+    },
+
+    /**
+     * 奖励-无尽宝箱x3
+     */
+    activeEndlessReward: async () => {
+        const ids = [71, 72, 73]
+        for (const id of ids) {
+            const payload = PAYLOADS['activeEndlessReward']
+            payload.id = id
+            log('processing', `开始领取无尽宝箱:${id}`)
+            let result = await requests.pf(payload)
+            if (result.head.state == 0) {
+                log('success', `领取成功`)
+                _logGain(result.head.gain)
+            } else {
+                log('error', `领取失败:${result.head.msg}`)
+            }
+            await common.sleep(1000)
+        }
+    },
+
+    /**
+     * 奖励-体力x100
+     */
+    activeEnergyReward: async () => {
+        const payload = PAYLOADS['activeEnergyReward']
+        log('processing', `开始领取体力x100`)
+        let result = await requests.pf(payload)
+        if (result.head.state == 0) {
+            log('success', `领取成功`)
+            _logGain(result.head.gain)
+        } else {
+            log('error', `领取失败:${result.head.msg}`)
+        }
+    },
+
+    /**
+     * 活跃度5档
+     */
+    activeReward: async () => {
+        const indexS = [0, 1, 2, 3, 4]
+        for (const index of indexS) {
+            const payload = PAYLOADS['activeReward']
+            payload.index = index
+            log('processing', `开始领取活跃度:${index + 1}`)
+            let result = await requests.pf(payload)
+            if (result.head.state == 0) {
+                log('success', `领取成功`)
+                _logGain(result.head.gain)
+            } else {
+                log('error', `领取失败:${result.head.msg}`)
+            }
+            await common.sleep(1000)
+        }
+    },
+
+    /**
+     * VIP每日奖励
+     */
+    dailyVipReward: async (args: {level: number}) => {
+        const ids = [100, 104, 105, 101, 106, 102, 103, 107]
+        if (args.level > ids.length) {
+            args.level = ids.length
+        }
+        if (args.level < 1) {
+            args.level = 1
+        }
+        for (let i = 0; i < args.level; i++) {
+            const id = ids[i]
+            const payload = PAYLOADS['dailyVipReward']
+            payload.id = id
+            log('processing', `开始领取VIP每日奖励:${i+1}`)
+            let result = await requests.pf(payload)
+            if (result.head.state == 0) {
+                log('success', `领取成功`)
+                _logGain(result.head.gain)
+            } else {
+                log('error', `领取失败:${result.head.msg}`)
+            }
+            await common.sleep(1000)
+        }
+    },
+
+    /**
+     * CDK兑换
+     */
+    cdkDo: async () => {
+        const payload = PAYLOADS['cdkDo']
+        const cdkS = settingStore.cdk.split('\n')
+        for (const cdk of cdkS){
+            if (!cdk || cdk.length == 0) {
+                continue
+            }
+            if (settingStore.usedCdk.includes(cdk)) {
+                log('warning', `跳过:${cdk}已兑换`)
+                continue
+            }
+            log('processing', `开始兑换:${cdk}`)
+            payload.cdkeyCode = cdk
+            let result = await requests.pf(payload)
+            if (result.head.state == 0) {
+                settingStore.usedCdk.push(cdk)
+                log('success', `兑换成功`)
+                _logGain(result.head.gain)
+            } else {
+                log('error', `兑换失败:${result.head.msg}`)
+            }
+            await common.sleep(1000)
+        }
+    }
 }
--- frontend/src/views/feature/daily/Index.vue
+++ frontend/src/views/feature/daily/Index.vue
@@ -8,6 +8,8 @@ const statusStore = useStatusStore();
 const settingStore = useSettingStore();
 const customStyle =
     'background: #f7f7f7;border-radius: 4px;margin-bottom: 12px;border: 0;overflow: hidden';
+
+
 </script>
 
 <template>
@@ -19,26 +21,79 @@ const customStyle =
     <template #expandIcon="{ isActive }">
       <caret-right-outlined :rotate="isActive ? 90 : 0"/>
     </template>
-    <a-collapse-panel key="1" header="免广告" :style="customStyle">
-      <a-space>
+    <a-collapse-panel key="freeAd" header="免广告" :style="customStyle">
+      <a-space wrap>
         <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyShopFree')">商城免费资源
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyEnergyReward', {
+          num: 1
+        })">每日广告体力x1
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyEnergyReward', {
+          num: 4
+        })">每日广告体力x4
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyEquipReward')">寻宝装备宝箱
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyExploreReward')">星际探索
         </a-button>
       </a-space>
     </a-collapse-panel>
-    <a-collapse-panel key="2" header="一键抽奖" :style="customStyle">
-      <a-space class="feature-daily-space">
+    <a-collapse-panel key="dailyGet" header="每日领取" :style="customStyle">
+      <a-space wrap>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyWheelReward', {
+          num: 1
+        })">寻宝转盘
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('sendDanMu')">弹幕钻石</a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('desktopStart')">桌面启动钻石
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyHelpGold')">助战金币
+        </a-button>
+        <br>
+        <a-space class="feature-daily-space" wrap>
+          <a-input-number v-model:value="settingStore.vipLevel" prefix="等级:"/>
+          <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyVipReward', {
+            level: settingStore.vipLevel
+          })">贵族奖励
+          </a-button>
+        </a-space>
+      </a-space>
+    </a-collapse-panel>
+    <a-collapse-panel key="activeReward" header="活跃奖励" :style="customStyle">
+      <a-space wrap>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('activeEndlessReward')">
+          奖励-无尽宝箱x3
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('activeEnergyReward')">
+          奖励-体力x100
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('activeReward')">活跃度奖励
+        </a-button>
+      </a-space>
+    </a-collapse-panel>
+    <a-collapse-panel key="cdkDo" header="CDK兑换" :style="customStyle">
+      <a-textarea v-model:value="settingStore.cdk" placeholder="请输入CDK"/>
+      <br>
+      <a-button style="margin-top: 5px" :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('cdkDo')">兑换</a-button>
+    </a-collapse-panel>
+    <a-collapse-panel key="clickGet" header="一键抽奖" :style="customStyle">
+      <a-space class="feature-daily-space" wrap>
         <a-input-number v-model:value="settingStore.dailyBlueBoxNum" prefix="次数:"/>
         <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyWheelReward', {
           num: settingStore.dailyBlueBoxNum
-        })">寻宝转盘</a-button>
-        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyBlueBoxReward', {
+        })">寻宝转盘
+        </a-button>
+        <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyEquipBoxReward', {
           num: settingStore.dailyBlueBoxNum,
           type: 21
-        })">蓝色宝箱</a-button>
+        })">蓝色宝箱
+        </a-button>
         <a-button :loading="statusStore.getBtnLoading()" @click="ApiFactory.execute('dailyBlueBoxReward', {
           num: settingStore.dailyBlueBoxNum,
           type: 22
-        })">紫色宝箱</a-button>
+        })">紫色宝箱
+        </a-button>
       </a-space>
     </a-collapse-panel>
   </a-collapse>
